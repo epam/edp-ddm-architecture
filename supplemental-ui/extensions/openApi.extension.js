28d949dd122f1889150885c4385da42dcca78e87
Mykyta_Holkin <mykyta_holkin@epam.com>
2023-01-16 18:16:03 +0200

    [EDP-DDM-22934] added open-api (swagger) extension
    Change-Id: If405b0f62d82322257cd8d8dc3c8bde9260fdebb

---
+++ supplemental-ui/extensions/openApi.extension.js
@@ -0,0 +1,47 @@
+function uniqueid(){
+  // always start with a letter (for DOM friendlyness)
+  var idstr=String.fromCharCode(Math.floor((Math.random()*25)+65));
+  do {
+    // between numbers and characters (48 is 0 and 90 is Z (42-48 = 90)
+    var ascicode=Math.floor((Math.random()*42)+48);
+    if (ascicode<58 || ascicode>64){
+      // exclude all chars between : (58) and @ (64)
+      idstr+=String.fromCharCode(ascicode);
+    }
+  } while (idstr.length<32);
+
+  return (idstr);
+}
+
+// usage example: openApi::{attachmentsdir}/architecture-workspace/platform-evolution/bp-groups/rrm-swagger.yml[My swagger]
+
+module.exports = function (registry) {
+  registry.blockMacro('openApi', function () {
+    var self = this
+    self.process(function (parent, target, args) {
+      var id = uniqueid();
+      var html = `
+      <details>
+        <summary class="title">${args?.title}</summary>
+        <div class="content">
+            <div id="swagger-ui_${id}"></div>
+        </div>
+      </details>
+      
+      <script>
+        document.addEventListener('DOMContentLoaded', function() {
+          SwaggerUIBundle({
+            url: "${target}",
+            dom_id: "#swagger-ui_${id}",
+            presets: [
+              SwaggerUIBundle.presets.apis,
+              SwaggerUIStandalonePreset
+            ]
+          })
+        }, false);
+      </script>
+      `;
+      return self.createBlock(parent, 'pass', html)
+    })
+  })
+}
