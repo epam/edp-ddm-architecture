2a943961d7ef1b521b8801724b1a10f121c3b600
Maksym_Kharchenko <maksym_kharchenko@epam.com>
2023-05-04 10:20:13 +0300

    [EDP-DDM-25346] Cosmetic changes to documentation
    Change-Id: I18d542e6aa44920527eb6ece8692cce2c6883dc4

--- supplemental-ui/static/language-selector/language-selector.js
+++ supplemental-ui/static/language-selector/language-selector.js
@@ -1,12 +1,11 @@
-!function() {
-  "use strict";
-  var t, e = document.querySelector(".page-languages .language-menu-toggle");
-  e && (t = document.querySelector(".page-languages"),
-      e.addEventListener("click", function(e) {
-        t.classList.toggle("is-active"),
-            e.stopPropagation()
-      }),
-      document.documentElement.addEventListener("click", function() {
-        t.classList.remove("is-active")
-      }))
-}();
+document.addEventListener('DOMContentLoaded', function() {
+    var t, e = document.querySelector(".page-languages .language-menu-toggle");
+    e && (t = document.querySelector(".page-languages"),
+        e.addEventListener("click", function(e) {
+            t.classList.toggle("is-active"),
+                e.stopPropagation()
+        }),
+        document.documentElement.addEventListener("click", function() {
+            t.classList.remove("is-active")
+        }))
+});
