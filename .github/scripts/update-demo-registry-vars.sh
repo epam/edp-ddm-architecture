b18e0dad1ef35e5ff11b072c3d0ff7b8ef341d6e
Oleh Boreiko1 <oleh_boreiko1@epam.com>
2023-08-31 09:20:26 +0000

    [EDP-DDM-27935] Added setting of demo-registry's vars for github publishing
    Change-Id: I5e84c39a74263a0ee841a9e91a86910666df8511

---
+++ .github/scripts/update-demo-registry-vars.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+find ./github-pages -name '*.html' -exec sed -i "s/{{{registry-name}}}/demo-registry/g" {} \;
+find ./github-pages -name '*.html' -exec sed -i "s/{{{cluster-name}}}/cluster-name/g" {} \;
+find ./github-pages -name '*.html' -exec sed -i "s/{{{dns-wildcard}}}/domain.com/g" {} \;
