9ce4f5fa4b5de666614c5430b0253f4515db0d00
jenkins <jenkins@epam.com>
2023-10-25 07:52:13 +0300

    [EDP-DDM-0000] Bulk update 1.9.8

--- .github/scripts/update-demo-registry-vars.sh
+++ .github/scripts/update-demo-registry-vars.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 find ./github-pages -name '*.html' -exec sed -i "s/{{{registry-name}}}/demo-registry/g" {} \;
+find ./github-pages -name '*.html' -exec sed -i "s/{{{stage-name}}}/main/g" {} \;
 find ./github-pages -name '*.html' -exec sed -i "s/{{{cluster-name}}}/cluster-name/g" {} \;
 find ./github-pages -name '*.html' -exec sed -i "s/{{{dns-wildcard}}}/domain.com/g" {} \;
